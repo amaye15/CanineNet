d708ee3a90a29cde8d3f97a4c9a505182c9e9701
amaye15 <70704207+amaye15@users.noreply.github.com>
2024-01-22 13:12:47 +0000

    app update

--- app.py
+++ app.py
@@ -1,37 +1,47 @@
-
 # Importing necessary libraries
 import streamlit as st
 from PIL import Image
 from transformers import ViTFeatureExtractor, ViTForImageClassification
-import requests
+import torch
+import plotly.express as px
 
 # Load the model and the feature extractor
 feature_extractor = ViTFeatureExtractor.from_pretrained('google/vit-base-patch16-224')
 model = ViTForImageClassification.from_pretrained('google/vit-base-patch16-224')
 
-# Function to classify the image
+# Function to classify the image and get sorted probabilities
 def classify_image(image):
-    # Convert the PIL image to RGB format if it's not
     if image.mode != 'RGB':
         image = image.convert('RGB')
 
-    # Resize the image to the size expected by the model (224x224 for ViT)
-    image = image.resize((224, 224))
-
-    # Process the image with the feature extractor
+    #image = image.resize((224, 224))
     inputs = feature_extractor(images=image, return_tensors="pt")
     outputs = model(**inputs)
     logits = outputs.logits
-    predicted_class_idx = logits.argmax(-1).item()
-    return model.config.id2label[predicted_class_idx]
+
+    # Convert logits to probabilities
+    probs = torch.nn.functional.softmax(logits, dim=-1)[0]
+
+    # Sort probabilities and labels
+    sorted_indices = torch.argsort(probs, descending=True)
+    sorted_probs = probs[sorted_indices].detach().numpy()
+    sorted_labels = [model.config.id2label[idx.item()] for idx in sorted_indices]
+
+    return sorted_probs, sorted_labels
+
+# Function to plot the bar chart using Plotly with classes on the y-axis
+def plot_bar_chart(sorted_probs, sorted_labels):
+    top_n = 9
+    fig = px.bar(y=sorted_labels[:top_n], x=sorted_probs[:top_n], labels={'y':'Class', 'x':'Probability'}, title='Class Probabilities')
+    fig.update_layout(yaxis={'categoryorder':'total ascending'})
+    st.plotly_chart(fig)
+
 
 # Creating the Streamlit interface
-st.title('Image Classification App')
+st.title('CanineNet')
 uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "png", "jpeg"])
 if uploaded_file is not None:
     image = Image.open(uploaded_file)
     st.image(image, caption='Uploaded Image.', use_column_width=True)
-    st.write("")
-    st.write("Classifying...")
-    label = classify_image(image)
-    st.write(f'Prediction: {label}')
+    sorted_probs, sorted_labels = classify_image(image)
+    plot_bar_chart(sorted_probs, sorted_labels)
